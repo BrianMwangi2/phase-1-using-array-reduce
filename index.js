868ba8bcb66e4d50d3cd73ec9898160dfa626148
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-03 13:10:07 +0300

    calculates sum of array

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+ // Example batteryBatches array
+const totalBatteries = batteryBatches.reduce((total, current) => total + current, 0);
+
+console.log(totalBatteries); // Output will be the sum of all battery amounts
+
 
 // Code your solution here
